ed565ccd780c3b4578826d180ca3c500b10de1d3
trackme-ddisley <declan.disley@track-me.app>
2026-01-23 17:01:43 +0000

    feat: Created userTable component and temporarily added to home page.

--- apps/frontend/src/modules/home/frames/home.frame.tsx
+++ apps/frontend/src/modules/home/frames/home.frame.tsx
@@ -1,9 +1,11 @@
 import { type FC } from 'react';
+import { UserTable } from '../../common/components/users/userTable.component';
 
 export const HomeFrame: FC = () => {
     return (
-        <>
+        <div style={{ padding: '2rem' }}>
             <h1>Home Page</h1>
-        </>
+            <UserTable />
+        </div>
     );
 };
